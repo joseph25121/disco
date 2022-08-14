0ae98d21c1fc2a6f334f8d94b27c5e716d8cc7a2
joseph25121 <101104157+joseph25121@users.noreply.github.com>
2022-08-14 12:28:39 +0530

    hi

--- doc.py
+++ doc.py
@@ -1,8 +1,7 @@
 def Add(a, b):
-       return a + b
-
-def sayHello():
-       prinnt("hello")
+           return a + b
+def SayHello():
+           print("hello")
 
 if __name__== '__main__':
-       sayHello()
+           SayHello()
